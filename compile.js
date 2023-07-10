83645aa823c52015da7b9cee19c4f202d6af4725
alishan <optimist.alishan@gmail.com>
2023-07-11 01:22:09 +0500

    compiling

--- compile.js
+++ compile.js
@@ -1,7 +1,8 @@
+const solc = require('solc');
 const path = require('path');
 const fs = require('fs');
 
 const inboxPath = path.resolve(__dirname, 'contracts', "Inbox.sol");
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-console.log(solc.compule(source, 1));
+console.log(solc.compile(source, 1));
