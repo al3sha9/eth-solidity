db99a78c0db263e297131245d3593646503d748e
alishan <optimist.alishan@gmail.com>
2023-07-13 01:07:47 +0500

    testing donw

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -3,5 +3,31 @@ const assert = require('assert');
 const {Web3} = require('web3');
 
 const ganache = require('ganache');
+const { it } = require('mocha');
 
 const web3 = new Web3(ganache.provider());
+
+// class Car{
+//     park(){
+//         return 'stopped';
+//     }
+//     drive(){
+//         return 'vroom';
+//     }
+// }
+
+// let car;
+
+// beforeEach(()=>{
+//     car = new Car();
+// })
+
+// describe('Car', ()=>{
+//     it('can park', ()=>{
+//         assert.equal(car.park(), 'stopped');
+//     });
+
+//     it('can drive', ()=>{
+//         assert.equal(car.drive(), 'vroom')
+//     })
+// });
