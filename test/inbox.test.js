eb2a1d5a54c1ac593ef5b2709699093cfebd58d2
alishan <optimist.alishan@gmail.com>
2023-07-14 00:05:24 +0500

    testing and sending eth

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,11 +1,40 @@
 const assert = require('assert');
-
 const {Web3} = require('web3');
-
 const ganache = require('ganache');
-const { it } = require('mocha');
-
+const { it, beforeEach, describe } = require('mocha');
 const web3 = new Web3(ganache.provider());
+const {interface, bytecode} = require('../compile');
+
+
+
+let accounts;
+let inbox;
+
+beforeEach(async () =>{
+    // get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+    
+
+    // use one of those accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode})
+    .send({ from: accounts[0], gas: '1000000' });
+})
+
+
+describe('Inbox', ()=>{
+    it('deploys a contract', ()=>{
+console.log(inbox)
+    })
+})
+
+
+
+
+
+
+
+
 
 // class Car{
 //     park(){
