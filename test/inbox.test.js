edd2e66111b702d4359daeac6e8d26ef965c9531
alishan <optimist.alishan@gmail.com>
2023-07-15 03:17:54 +0500

    tests final

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -26,6 +26,11 @@ describe('Inbox', ()=>{
     it('deploys a contract', ()=>{
         assert.ok(inbox.options.address)
     })
+
+    it('has a default message', async ()=>{
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hi There!')
+    })
 })
 
 
