b253775add19a0dc471677204887d1385dae589e
alishan <optimist.alishan@gmail.com>
2023-07-18 00:31:04 +0500

    pushhh

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -31,6 +31,12 @@ describe('Inbox', ()=>{
         const message = await inbox.methods.message().call();
         assert.equal(message, 'Hi There!')
     })
+
+    it('Changes the message', async ()=>{
+        await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'bye')
+    })
 })
 
 
